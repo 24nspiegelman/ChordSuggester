542095acf93fdcd768d951574c02da389f79268c
24nspiegelman <24nspiegelman@athenian.org>
2024-04-09 10:28:53 -0700

    Create note class and created all notes

---
+++ NotesClass.js
@@ -0,0 +1,18 @@
+class Note {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const G = new Note("G");
+const GSharpAFlat = new Note("G#/A♭");
+const A = new Note("A");
+const ASharpBFlat = new Note("A#/B♭");
+const B = new Note("B");
+const C = new Note("C");
+const CSharpDFlat = new Note("C#/D♭");
+const D = new Note("D")
+const DSharpEFlat = new Note("D#/E♭");
+const E = new Note ("E");
+const F = new Note("F");
+const FSharpGFlat = new Note("F#/G♭");
