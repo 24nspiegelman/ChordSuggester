469af973be1c891907942a5b7470ad65b3bcaa28
24nspiegelman <24nspiegelman@athenian.org>
2024-04-16 10:41:53 -0700

    Changed Scroll to Dropdown

--- App.js
+++ App.js
@@ -3,43 +3,51 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { Dropdown } from 'react-native-material-dropdown-v2-fixed';
 import React,{useState} from 'react';
+import { SelectList } from 'react-native-dropdown-select-list';
 
 export default function App() {
-const [key, setKey] = useState();
-  return (
-    <View style={styles.pickerStyles}>
-      <Picker
-      selectedValue={key}
-      onValueChange={(itemValue, itemIndex) =>
-      setKey(itemValue)
-    }>
-  <Picker.Item label="A" value="A" />
-  <Picker.Item label="A#" value="A#" />
-  <Picker.Item label="B♭" value="B♭" />
-  <Picker.Item label="B" value="B" />
-  <Picker.Item label="C" value="C" />
-  <Picker.Item label="C#" value="C#" />
-  <Picker.Item label="D♭" value="D♭" />
-  <Picker.Item label="D" value="D" />
-  <Picker.Item label="D#" value="D#" />
-  <Picker.Item label="E♭" value="E♭" />
-  <Picker.Item label="E" value="E" />
-  <Picker.Item label="F" value="F" />
-  <Picker.Item label="F#" value="F#" />
-  <Picker.Item label="G♭" value="G♭" />
-  <Picker.Item label="G" value="G" />
-  <Picker.Item label="G#" value="G#" />
-  <Picker.Item label="A♭" value="A♭" />
 
-</Picker>
-    </View>
-  );
+  const [selected, setSelected] = React.useState("");
+
+
+
+  const data = [
+    {key: 'A♭', value:'A♭'},
+    {key: 'A', value:'A'},
+    {key: 'A#', value:'A#'},
+    {key: 'B♭', value:'B♭'},
+    {key: 'B', value:'B'},
+    {key: 'C', value:'C'},
+    {key: 'C#', value:'C#'},
+    {key: 'D♭', value:'D♭'},
+    {key: 'D', value:'D'},
+    {key: 'D#', value:'D#'},
+    {key: 'E♭', value:'E♭'},
+    {key: 'E', value:'E'},
+    {key: 'F', value:'F'},
+    {key: 'F#', value:'F#'},
+    {key: 'G♭', value:'G♭'},
+    {key: 'G', value:'G'},
+    {key: 'G#', value:'G#'},
+    
+  
+
+
+  ];
+return (
+  <View style={styles.dropdownStyles}>
+    <SelectList data={data} setSelected={setSelected} placeholder='Select Key'>
+
+    </SelectList>
+  </View>
+)
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -51,5 +59,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: '15%',
     marginLeft: '21%',
+  },
+  dropdownStyles:{
+    paddingHorizontal:20,
+    paddingVertical:50, 
+    flex: 1
   }
 });
