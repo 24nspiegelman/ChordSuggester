38329fecf3339183b9b0924ce8f72d60c2bbfd68
24nspiegelman <24nspiegelman@athenian.org>
2024-04-22 21:42:30 -0700

    keyBuilder still not working

--- KeyBuilder.js
+++ KeyBuilder.js
@@ -1,4 +1,23 @@
-import noteArray from './NotesClass.js';
+class Note {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const G = new Note("G");
+const GSharpAFlat = new Note("G#/A♭");
+const A = new Note("A");
+const ASharpBFlat = new Note("A#/B♭");
+const B = new Note("B");
+const C = new Note("C");
+const CSharpDFlat = new Note("C#/D♭");
+const D = new Note("D")
+const DSharpEFlat = new Note("D#/E♭");
+const E = new Note ("E");
+const F = new Note("F");
+const FSharpGFlat = new Note("F#/G♭");
+
+let noteArray = [A, ASharpBFlat, B, C, CSharpDFlat, D, DSharpEFlat, E, F, FSharpGFlat, G, GSharpAFlat];
 
 
 
@@ -12,10 +31,9 @@ export default function keyBuilder(key, scale) {
         steps = minStep
     }
     let noteIndex = 0;
-    while (noteArray[noteIndex] !== key){
+    while (noteArray[noteIndex].name !== key){
         noteIndex++;
-        if(noteIndex > 5){
-            console.log('Too Far');
+        if(noteIndex > 11){
             break;
         }
     }
@@ -25,6 +43,7 @@ export default function keyBuilder(key, scale) {
             noteIndex = noteIndex%11 - 1;
         }
         keyArray[i] = noteArray[noteIndex];
+        console.log("noteArray[noteIndex]: " + noteArray[noteIndex]);
         noteIndex += steps[stepsIndex];
         stepsIndex++;
     }
