c3147304f9b285b58074e13f254e6c7041175d01
24nspiegelman <24nspiegelman@athenian.org>
2024-04-22 22:12:18 -0700

    Added getters/setters for KeyClass

--- KeyClass.js
+++ KeyClass.js
@@ -3,4 +3,21 @@ export default class Key {
         this.notes = notes;
         this.chords = chords;
     }
+
+
+    get notes(){
+        return this.notes;
+    }
+
+    set notes(notes){
+        this.notes = notes;
+    }
+
+    get chords(){
+        return this.chords;
+    }
+
+    set chords(chords){
+        this.chords = chords;
+    }
 }
