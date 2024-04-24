98eea49eabe04f549f4d80b5c9e6736cd4a2d179
24nspiegelman <24nspiegelman@athenian.org>
2024-04-24 14:28:21 -0700

    Need to restructure how keyBuilder will build scales

--- KeyBuilder.js
+++ KeyBuilder.js
@@ -5,27 +5,32 @@ class Note {
 }
 
 const G = new Note("G");
-const GSharpAFlat = new Note("G#/A♭");
+const GSharp = new Note("G#");
+const AFlat = new Note("A♭")
 const A = new Note("A");
-const ASharpBFlat = new Note("A#/B♭");
+const ASharp = new Note("A#");
+const BFlat = new Note("B♭")
 const B = new Note("B");
 const C = new Note("C");
-const CSharpDFlat = new Note("C#/D♭");
+const CSharp = new Note("C#");
+const DFlat = new Note("D♭")
 const D = new Note("D")
-const DSharpEFlat = new Note("D#/E♭");
+const DSharp = new Note("D#");
+const EFlat = new Note("E♭");
 const E = new Note ("E");
 const F = new Note("F");
-const FSharpGFlat = new Note("F#/G♭");
+const FSharp = new Note("F#/G♭");
+const GFlat = new Note("G♭");
 
-let noteArray = [A, ASharpBFlat, B, C, CSharpDFlat, D, DSharpEFlat, E, F, FSharpGFlat, G, GSharpAFlat];
+let sharpNoteArray = [A, ASharp, B, C, CSharp, D, DSharp, E, F, FSharp, G, GSharp];
+let flatNoteArray = [A, BFlat, B, C, DFlat, D, EFlat, E, F, GFlat, G, AFlat];
 
 
 
 export default function keyBuilder(key, scale) {
     let keyArray = new Array(7);
     let steps = [];
-    console.log("key: " + key);
-    console.log("scale: " + scale);
+    let noteArray = [];
     if(scale === "Major"){
         steps = majStep;
     }
@@ -39,26 +44,23 @@ export default function keyBuilder(key, scale) {
             break;
         }
     }
-    console.log("noteIndex: " + noteIndex)
-    console.log("noteArray[noteIndex]1: " + noteArray[noteIndex].name)
     let stepsIndex = 0;
     for(let i = 0; i < 7; i++){
         if(noteIndex >11){
             noteIndex = noteIndex%11 - 1;
         }
         keyArray[i] = noteArray[noteIndex];
-        console.log("noteArray[noteIndex]: " + noteArray[noteIndex].name);
         noteIndex += steps[stepsIndex];
         stepsIndex++;
     }
 
-    console.log("0: " + keyArray[0].name);
-    console.log("1: " + keyArray[1].name);
-    console.log("2: " + keyArray[2].name);
-    console.log("3: " + keyArray[3].name);
-    console.log("4: " + keyArray[4].name);
-    console.log("5: " + keyArray[5].name);
-    console.log("6: " + keyArray[6].name);
+    // console.log("0: " + keyArray[0].name);
+    // console.log("1: " + keyArray[1].name);
+    // console.log("2: " + keyArray[2].name);
+    // console.log("3: " + keyArray[3].name);
+    // console.log("4: " + keyArray[4].name);
+    // console.log("5: " + keyArray[5].name);
+    // console.log("6: " + keyArray[6].name);
     return keyArray;
 }
 
