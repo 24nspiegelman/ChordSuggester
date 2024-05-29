8ee1195d45c2591cd0ba91111b1e7c00fd4366a4
24nspiegelman <24nspiegelman@athenian.org>
2024-05-29 14:26:42 -0700

    Got everything to mostly work. Now just need chords to show when pushed

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity, StyleSheet, Text, View, SafeAreaView, Modal, Button, Pressable} from 'react-native';
-import React,{useState, useCallback, useMemo, useRef} from 'react';
+import { TouchableOpacity, StyleSheet, Text, View, SafeAreaView, Modal, Button, Pressable } from 'react-native';
+import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
 import { SelectList } from 'react-native-dropdown-select-list';
 import keyBuilder from './KeyBuilder';
 import Sheet from 'react-modal-sheet';
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomSheet, { BottomSheetSectionList, BottomSheetModal, BottomSheetView, BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import chordBuilder from './ChordBuilder.js';
 import { Chord, Interval, Note, Scale, Key, ScaleType } from "tonal";
@@ -12,83 +12,74 @@ import { Chord, Interval, Note, Scale, Key, ScaleType } from "tonal";
 
 export default function App() {
   const [key, setKey] = React.useState("");
-  const [type, setType] = React.useState("")
+  const [type, setType] = React.useState("");
+  const [chosenScale, setChosenScale] = React.useState(Scale.get("C Major"));
+
   const keys = [
-    {key: 'A♭', value:'A♭'},
-    {key: 'A', value:'A'},
-    {key: 'A#', value:'A#'},
-    {key: 'B♭', value:'B♭'},
-    {key: 'B', value:'B'},
-    {key: 'C', value:'C'},
-    {key: 'C#', value:'C#'},
-    {key: 'D♭', value:'D♭' },
-    {key: 'D', value:'D'},
-    {key: 'D#', value:'D#'},
-    {key: 'E♭', value:'E♭'},
-    {key: 'E', value:'E'},
-    {key: 'F', value:'F'},
-    {key: 'F#', value:'F#'},
-    {key: 'G♭', value:'G♭'},
-    {key: 'G', value:'G'},
-    {key: 'G#', value:'G#'},
-    
-  ];
-    const types = [
-      {key: 'Major', value: 'Major'},
-      {key: 'Minor', value: 'Major'},
-    ]
-    const scales = [
-      {key: 'Major' ,value: 'Major'},
-      {key: 'Minor', value: 'Minor'},
-      {key: 'Dorian', value: 'Dorian'},
-      {key: 'Phrygian', value: 'Phrygian'},
-      {key: 'Lydian', value: 'Lydian'},
-      {key: 'Mixolydian', value: 'Mixolydian'},
-      {key: 'Locrian', value: 'Locrian'},
-      {key: 'Harmonic Major', value: 'Harmonic Major'},
-      {key: 'Harmonic Minor', value: 'Harmonic Minor'},
-      {key: 'Hungarian Minor', value: 'Hungarian Minor'},
-      {key: 'Jazz Minor', value: 'Jazz Minor'},
-      {key: 'Major Blues', value: 'Major Blues'},
-      {key: 'Minor Blues', value: 'Minor Blues'},
-      {key: 'Prometheus', value: 'Prometheus'},
-      {key: 'Augmented', value: 'Augmented'},
-      {key: 'Diminished', value: 'Diminished'},
-      {key: 'Aeolian Dominant', value: 'Aeolian Dominant'},
-      {key: 'Phrygian Dominant', value: 'Phrygian Dominant'},
-      {key: 'Whole Tone', value: 'Whole Tone'},
-      {key: 'Neapolitan Major', value: 'Neapolitan Major'},
-      {key: 'Neapolitan Minor', value: 'Neapolitan Minor'},
-      {key: 'Persian', value: 'Persian'},
-      {key: 'Double Harmonic Major', value: 'Double Harmonic Major'},
-      {key: 'Altered Dominant', value: 'Altered Dominant'},
-      {key: 'Major Pentatonic', value: 'Major Pentatonic'},
-      {key: 'Minor Pentatonic', value: 'Minor Pentatonic'},
-      {key: 'Neutral Pentatonic', value: 'Neutral Pentatonic'},
- 
+    { key: 'A♭', value: 'A♭' },
+    { key: 'A', value: 'A' },
+    { key: 'A#', value: 'A#' },
+    { key: 'B♭', value: 'B♭' },
+    { key: 'B', value: 'B' },
+    { key: 'C', value: 'C' },
+    { key: 'C#', value: 'C#' },
+    { key: 'D♭', value: 'D♭' },
+    { key: 'D', value: 'D' },
+    { key: 'D#', value: 'D#' },
+    { key: 'E♭', value: 'E♭' },
+    { key: 'E', value: 'E' },
+    { key: 'F', value: 'F' },
+    { key: 'F#', value: 'F#' },
+    { key: 'G♭', value: 'G♭' },
+    { key: 'G', value: 'G' },
+    { key: 'G#', value: 'G#' },
   ];
 
-  let chosenScale = Scale.get("C Major");
-  if(!(key === '') && !(scale === '')){
-    if(scale.contains("Major")){
-      chosenScale = Scale.get(key + " " + scale);
+  const types = [
+    { key: 'Major', value: 'Major' },
+    { key: 'Minor', value: 'Minor' },
+  ];
+
+  useEffect(() => {
+    if (key !== '' && type !== '') {
+      setChosenScale(Scale.get(`${key} ${type}`));
+    }
+  }, [key, type]);
+
+  function getChord(i) {
+    let root = chosenScale.notes[i];
+    console.log(root);
+    i += 2;
+    if (i > 6) {
+      i = i % 7;
+    }
+    let third = chosenScale.notes[i];
+    console.log(third);
+    i += 2;
+    if (i > 6) {
+      i = i % 7;
+    }
+    let fifth = chosenScale.notes[i];
+    console.log(fifth);
+
+    const chord = Chord.detect([root, third, fifth]);
+    if (chord.length > 0) {
+      return chord[0];
+    } else {
+      return "Unknown";
     }
   }
 
- 
   const sheetRef = useRef(null);
 
-
-  const sections = 
-    [
-    // {title: "Triad", data:[getChord(0), getChord(1), getChord(2), getChord(3), getChord(4), getChord(5), getChord(6)]
-    // },
-    {title: "Triad", data:[chosenScale.notes[0], chosenScale.notes[1], chosenScale.notes[2], chosenScale.notes[3], chosenScale.notes[4], chosenScale.notes[5], chosenScale.notes[0]]
+  const sections = [
+    {
+      title: "Triad",
+      data: [getChord(0), getChord(1), getChord(2), getChord(3), getChord(4), getChord(5), getChord(6)]
     },
+  ];
 
-  ]
-
-  const snapPoints = useMemo(() => [ "55%", "85%", "90%"], []);
+  const snapPoints = useMemo(() => ["55%", "85%", "90%"], []);
 
   const handleSheetChange = useCallback((index) => {
     console.log("handleSheetChange", index);
@@ -100,12 +91,6 @@ export default function App() {
     sheetRef.current?.close();
   }, []);
 
-  let chordViewerPosition = 0;
-  let chordViewerArray = [];
-  // function chordPress(chord){
-
-  // }
-
   const renderSectionHeader = useCallback(
     ({ section }) => (
       <View style={styles.sectionHeaderContainer}>
@@ -114,82 +99,66 @@ export default function App() {
     ),
     []
   );
+
   const renderItem = useCallback(
     ({ item }) => (
       <View style={styles.itemContainer} key={item}>
         <TouchableOpacity style={styles.itemStyle}>
-        <Text style={styles.itemText}>{item}</Text>
+          <Text style={styles.itemText}>{item}</Text>
         </TouchableOpacity>
       </View>
     ),
     []
   );
 
-return (
-  <SafeAreaView style={styles.safeArea}>
-    <View style={styles.container}>
-      <View style={styles.dropdownStyles}>
-        <SelectList data={keys} setSelected={setKey} placeholder='Select Key' search={false} >
-
-        </SelectList>
-      </View>
-      <View style={styles.dropdownStyles}>
-        <SelectList data={types} setSelected={setType} placeholder='Select Type' >
-        </SelectList>
-      </View>
-    </View>
-    <View style={styles.chordViewerView}>
-    <View style={styles.chordViewer}>
-
-      <View style={styles.chordBox}>
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.dropdownStyles}>
+          <SelectList data={keys} setSelected={setKey} placeholder='Select Key' search={false} />
         </View>
-
-      <View style={styles.chordBox}>
-          </View>
-
-      <View style={styles.chordBox}>
+        <View style={styles.dropdownStyles}>
+          <SelectList data={types} setSelected={setType} placeholder='Select Type' />
         </View>
-      
-      <View style={styles.chordBox}>
+      </View>
+      <View style={styles.chordViewerView}>
+        <View style={styles.chordViewer}>
+          <View style={styles.chordBox}></View>
+          <View style={styles.chordBox}></View>
+          <View style={styles.chordBox}></View>
+          <View style={styles.chordBox}></View>
         </View>
-
       </View>
-      </View>
-    
-    <View style={styles.typeButtonView}>
-    <Pressable style={styles.typeButton} onPress={() => handleSnapPress(1)}>
-      <Text style={styles.typeButtonText}>Pick Chord</Text>
+      <View style={styles.typeButtonView}>
+        <Pressable style={styles.typeButton} onPress={() => handleSnapPress(1)}>
+          <Text style={styles.typeButtonText}>Pick Chord</Text>
         </Pressable>
       </View>
 
-  <GestureHandlerRootView>
-  <BottomSheet
-  ref={sheetRef}
-  index={-1}
-  snapPoints={snapPoints}
-  onChange={handleSheetChange}
->
-  <View style={styles.closeModalButtonView}>
-  <Pressable style={styles.closeModalButton} onPress={() => handleClosePress()} >
-    <Text style={styles.closeButtonText}> Close Window</Text>
-    </Pressable>
-    </View>
-  <BottomSheetSectionList
-    sections={sections}
-    keyExtractor={(i) => i}
-    renderSectionHeader={renderSectionHeader}
-    renderItem={renderItem}
-    contentContainerStyle={styles.contentContainer}
-  />
-</BottomSheet>
-</GestureHandlerRootView>
-  </SafeAreaView>
-    
-
-
-
-
-)}
+      <GestureHandlerRootView>
+        <BottomSheet
+          ref={sheetRef}
+          index={-1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChange}
+        >
+          <View style={styles.closeModalButtonView}>
+            <Pressable style={styles.closeModalButton} onPress={handleClosePress}>
+              <Text style={styles.closeButtonText}>Close Window</Text>
+            </Pressable>
+          </View>
+          <BottomSheetSectionList
+            sections={sections}
+            keyExtractor={(item, index) => item + index}
+            renderSectionHeader={renderSectionHeader}
+            renderItem={renderItem}
+            contentContainerStyle={styles.contentContainer}
+          />
+        </BottomSheet>
+      </GestureHandlerRootView>
+    </SafeAreaView>
+  );
+}
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
@@ -259,7 +228,6 @@ const styles = StyleSheet.create({
   },
 contentContainer: {
   backgroundColor: "white",
-  // flexDirection: 'row',
 },
 sectionHeaderContainer: {
   backgroundColor: "white",
@@ -269,7 +237,7 @@ sectionHeaderContainer: {
 itemContainer: {
   padding: 6,
   margin: 6,
-  width: 60,
+  width: '95%',
   height: 60,
   borderRadius: 10,
   backgroundColor: "#eee",
